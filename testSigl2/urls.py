7bfb06e96d3309bb6a2431cbf52b1c5a7067545d
AssemienDev <139700557+AssemienDev@users.noreply.github.com>
2025-05-12 11:36:55 +0000

    terminer

--- testSigl2/urls.py
+++ testSigl2/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import path, include, re_path
+from testSigl2 import settings
 
 from esatictest.views import index
 
